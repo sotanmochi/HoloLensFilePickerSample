58b229b71660c0cbc7f08100e65e63291cf9e20f
sotanmochi <stmczk@gmail.com>
2018-09-12 20:27:49 +0900

    Update script and settings

--- Assets/FilePicker.cs
+++ Assets/FilePicker.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-#if !UNITY_EDITOR && UNITY_WSA
+#if !UNITY_EDITOR && UNITY_WSA_10_0
 using System;
 using System.Threading.Tasks;
 using Windows.Storage.Pickers;
@@ -13,7 +13,9 @@ public class FilePicker : MonoBehaviour
 	void Start ()
 	{
 #if !UNITY_EDITOR && UNITY_WSA_10_0
+		Debug.Log("***********************************");
 		Debug.Log("File Picker start.");
+		Debug.Log("***********************************");
 
 		UnityEngine.WSA.Application.InvokeOnUIThread(async () =>
 		{
@@ -22,11 +24,21 @@ public class FilePicker : MonoBehaviour
 			var file = await filepicker.PickSingleFileAsync();
 			UnityEngine.WSA.Application.InvokeOnAppThread(() => 
 			{
-				Debug.Log((file != null) ? file.Name : "No data");
+				Debug.Log("***********************************");
+				string name = (file != null) ? file.Name : "No data";
+				Debug.Log("Name: " + name);
+				Debug.Log("***********************************");
+				string path = (file != null) ? file.Path : "No data";
+				Debug.Log("Path: " + path);
+				Debug.Log("***********************************");
+
 			}, false);
 		}, false);
 
+		
+		Debug.Log("***********************************");
 		Debug.Log("File Picker end.");
+		Debug.Log("***********************************");
 #endif
 	}
 }
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # FileOpenPicker sample for HoloLens
 
 ## Tested Environment
-- Unity 2017.4.8f1
+- Unity 2017.4.9f1
 - Visual Studio 2017
-- HoloLens RS5
+- HoloLens RS5 Preview (OS Build: 17720.1000)
 
 ## Reference
 - HoloLensのUnity内でFilepicker使ってみた (http://akihiro-document.azurewebsites.net/post/hololens_filepicker/)
